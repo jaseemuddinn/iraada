87ce64f592c463df01333d44ea6ebd19b7f1385a
jaseemuddin <jaseemuddinn@gmail.com>
2024-02-29 15:18:50 +0530

    edd

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import './about.css'
 
 function About() {
+<<<<<<< HEAD
+    
   return (
     <div>
-        
+        <div className="navbar">Navigation Bar</div>
+        <div className="home">home</div>
     </div>
   )
 }
