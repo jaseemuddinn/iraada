d22f9c9eb938880c8821c090cef2847fb3436432
jaseemuddin <jaseemuddinn@gmail.com>
2024-03-25 19:42:41 +0530

    updated

--- client/src/components/eventComponents/Theme/Theme.jsx
+++ client/src/components/eventComponents/Theme/Theme.jsx
@@ -14,36 +14,36 @@ function Theme() {
                 </span>
                 aims to bring together young leaders, activist, professionals, and stakeholders from around the world to address pressing environmental challenges. Through three fundamental pillars of Awareness, Action, and Advocacy, the summit seeks to mobilise youth to take a proactive role in safeguarding our planet's future.
             </p>
-            <div className='grid grid-cols-2 flex-wrap gap-3 text-white mt-5 text-center font-Montserrat font-medium text-xl'>
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
+            <div className='grid grid-cols-2 flex-wrap gap-3 text-white mt-5 text-center font-Montserrat font-bold text-xl'>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
                     Water
                 </div>
 
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
                     Climate Change
                 </div>
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
                     Circular Economy
                 </div>
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
                     Energy
                 </div>
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
                     Air
                 </div>
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
-                    Scope in EVS - Care, Academic and Research Prospects
-                </div>
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
                     Startup/Business Ecosystems
                 </div>
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
+                    Scope in EVS - Care, Academic and Research Prospects
+                </div>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
                     Youth Celebrity Champions and Green Influencers
                 </div>
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
                     Health
                 </div>
-                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500 hover:italic'>
+                <div className='rounded-full p-8 bg-gray-500 hover:bg-gradient-to-bl from-green-400 via-teal-400 to-blue-500'>
                     Wildlife Ecology
                 </div>
             </div>
